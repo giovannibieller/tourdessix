687df2d7439dd1d9d8a065a71bb8d7346c6f4875
Giovanni Bieller <giovanni.bieller@gmail.com>
2025-11-07 12:02:24 +0100

    feat: add favicons

--- includes/favicons.php
+++ includes/favicons.php
@@ -3,10 +3,12 @@
 ?>
 
 <!-- generated favicons block -->
-<link rel="apple-touch-icon" sizes="180x180" href="<?php echo $tmpDir;?>/dist/ico/apple-touch-icon.png">
-<link rel="icon" type="image/png" sizes="32x32" href="<?php echo $tmpDir;?>/dist/ico/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="16x16" href="<?php echo $tmpDir;?>/dist/ico/favicon-16x16.png">
-<link rel="manifest" href="<?php echo $tmpDir;?>/manifest.json">
-<link rel="mask-icon" href="<?php echo $tmpDir;?>/dist/ico/safari-pinned-tab.svg" color="#000000">
+<link rel="icon" type="image/png" href="<?php echo $tmpDir;?>/dist/ico/favicon-96x96.png" sizes="96x96" />
+<link rel="icon" type="image/svg+xml" href="<?php echo $tmpDir;?>/dist/ico/favicon.svg" />
+<link rel="shortcut icon" href="<?php echo $tmpDir;?>/dist/ico/favicon.ico" />
+<link rel="apple-touch-icon" sizes="180x180" href="<?php echo $tmpDir;?>/dist/ico/apple-touch-icon.png" />
+<meta name="apple-mobile-web-app-title" content="TD6" />
+<link rel="manifest" href="<?php echo $tmpDir;?>/dist/ico/site.webmanifest" />
+
 <meta name="msapplication-TileColor" content="#000000">
 <meta name="theme-color" content="<?php echo $seo_theme_color; ?>">
